2b3871f51793197de59b74817523735a8c426c6c
Rie Amasato <rie_amasato@icloud.com>
2024-11-19 01:36:55 +0000

    画面ロックを自動でさせない制御をしてみたい

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+index.vue
+{{isAllow}}
+</template>
+
+<script setup>
+const isAllow=ref("")
+
+onMounted(()=>{
+  if("wakeLock" in navigator){navigator.wakeLock.request("screen").then((res)=>{
+    isAllow.value="画面ロックさせない制御中"})
+  }else{
+    isAllow.value="画面ロック制御非対応"
+  }
+})
+
+</script>
